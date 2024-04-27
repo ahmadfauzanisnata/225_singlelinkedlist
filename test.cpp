43455053b61f69fbcb4126663b59c19c3a5be2f2
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-27 11:30:55 +0700

    Menambahkan kondisi while loop untuk melakukan pencarian node dalam linked list berdasarkan NIM.

--- test.cpp
+++ test.cpp
@@ -53,7 +53,7 @@ void addNode() {
 bool serachNode(int nim, Node* current, Node* previous){
     previous = START;
     current = START;
-    
+    while (current != NULL && nim > current->noMhs)
     {
 
     }
