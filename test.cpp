bc2dd7cd0fff20da3cb5cd37b1ff58a725b51bd2
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-27 11:36:45 +0700

    Menambahkan fungsi deleteNode untuk menghapus node dari linked list berdasarkan NIM yang diberikan.

--- test.cpp
+++ test.cpp
@@ -55,7 +55,24 @@ bool serachNode(int nim, Node* current, Node* previous){
     current = START;
     while (current != NULL && nim > current->noMhs)
     {
-
+        previous = current;
+        current = current->next;
+    }
+    if (current == NULL)
+    
+    {
+        return false;
+    }
+    else if (current ->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 
+}
+bool deleteNode(int nim){
+    
 }
