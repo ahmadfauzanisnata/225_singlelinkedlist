e2c378422c8a748a88460a9d6d2d343335bbda28
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-27 11:15:24 +0700

    Menambahkan kode untuk menyisipkan node baru di awal linked list.

--- test.cpp
+++ test.cpp
@@ -24,8 +24,13 @@ void addNode() {
             cout << "NIM sudah ada" << endl;
             return;
         }
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+
 
     }
+    
     
 
 
